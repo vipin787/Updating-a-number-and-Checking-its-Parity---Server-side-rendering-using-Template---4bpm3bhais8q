f1be1d5ce3b98bf7aa429e886c8949b8eba85b98
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-05-02 21:37:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,45 +3,47 @@ const app = express();
 
 app.use(express.json());
 
+ 
+//Complete below given Middleware function which check whether number provided in api as params is odd or Even . Example is shown below
 
-//Complete below given Middleware function which check whether number provided in api as params is odd or Even.
+//Example: 
+// GET Reqest of API  '/?num=18' --> The router should return {"num":"18","isOdd":false}
 
-const checkIsOdd = (req, res, next) => {
-  const { num } = req.query;
-  const isOdd = Number(num) % 2 !== 0;
-  req.query.isOdd = isOdd;
-  next();
-};
+function CheckisOdd(req, res, next) {
 
-//Complete below given Middleware function which adds 2 to a number provided in api as params.
+    //Write Your Code here
+ var value = parseInt(req.query.num);
+    if( value%2 == 0 ){
+        req.query.isOdd = false;
+    }else{
+        req.query.isOdd = true;
+    }
+    next();
 
-const add2 = (req, res, next) => {
-  const { num } = req.query;
-  req.query.num = Number(num) + 2;
-  next();
-};
+}
 
-/*
 
-Example :- 
-GET Reqest of API  '/?num=18' --> The router should return {"num": "20","isOdd":false}
+//app.get('/', CheckisOdd, (req, res) => {
+    
+    //num in data should be replaced by num from the get request route
+    //isOdd in data should be replaced by whether num is odd or even if it odd make it true else false 
+    //const data = {
+        //"num" : 5,
+        //"isOdd": true
+    //};
 
-*/
-
-//app.get('/', add2, CheckisOdd, (req, res) => {
-
-    //num in data should be replaced by (num query + 2) from the get request route
-    //isOdd in data should be replaced by whether (num query + 2) is odd or even if it odd make it true else false 
-  //  const data = {
-      //  "num" : "20",
-      //  "isOdd" : false
-   // };
-  //  res.send(JSON.stringify(data));
+    //res.send(JSON.stringify(data));
 //});
-app.get("/", add2, checkIsOdd, (req, res) => {
-  const { num, isOdd } = req.query;
-  const data = { num, isOdd };
-  res.json(data);
+
+
+app.get('/', CheckisOdd, (req, res) => {
+    const data = {
+        "num" : req.query.num,
+        "isOdd" : req.query.isOdd
+    };
+    res.send(JSON.stringify(data));
 });
+
+
 
 module.exports = app;
